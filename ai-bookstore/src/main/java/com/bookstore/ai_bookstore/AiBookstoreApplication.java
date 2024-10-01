22c3039e8466d76a39a240f1a9a9e36764e2cb92
gil <gilson1212@yahoo.com.br>
2024-10-01 15:37:39 -0300

    Spring-AI with WebFlux

---
+++ ai-bookstore/src/main/java/com/bookstore/ai_bookstore/AiBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.ai_bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiBookstoreApplication.class, args);
+	}
+
+}
